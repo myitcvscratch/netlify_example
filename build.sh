b831aa588e858d9f41b361abbe7de1c451df5dff
Paul Jolly <paul@myitcv.io>
2020-03-26 13:43:01 +0000

    Trying out cache dir

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ set -eu
 
 mkdir -p _functions
 go build -o ./_functions/hello-lambda ./functions/hello
+mkdir -p $NETLIFY_CACHE_DIR/our_node_modules
+ln -s $NETLIFY_CACHE_DIR/our_node_modules node_modules
+npm install
