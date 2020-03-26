3ad0c2abab80b756486aeb051e1f7449fe90dfe1
Paul Jolly <paul@myitcv.io>
2020-03-26 13:46:29 +0000

    Try again

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ env
 
 mkdir -p _functions
 go build -o ./_functions/hello-lambda ./functions/hello
-mkdir -p $NETLIFY_CACHE_DIR/our_node_modules
-ln -s $NETLIFY_CACHE_DIR/our_node_modules node_modules
+mkdir -p $NETLIFY_BUILD_BASE/cache/our_node_modules
+ln -s $NETLIFY_BUILD_BASE/cache/our_node_modules
 npm install
